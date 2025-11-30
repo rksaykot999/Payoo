093c294c9c7cb19095f3b90a85e3faab2dca4e90
rksaykot <rksaikatkhan999@gmail.com>
2025-11-30 22:09:53 +0600

    Add money and Cash out system ready.

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
-document.getElementById('login-btn').addEventListener('click', function(event){
-event.preventDefault();
-    let phone = document.getElementById('phone-number').value;
-    let password = document.getElementById('password').value;
-    password = parseInt(password);
-    if(phone.length == 11){
-        if(password == 123){
-           window.location.href = "main.html";
+document.getElementById('add-money-section').style.display = 'none';
+document.getElementById('cash-out-section').style.display = 'none';
+
+//Veriable diclaretion
+let latestPaymentSection = document.getElementById('latest-payment-section');
+let addMoneySection = document.getElementById('add-money-section');
+let cashOutSection = document.getElementById('cash-out-section');
+
+//Add Money button
+document.getElementById('add-money').addEventListener('click', function () {
+    latestPaymentSection.style.display = 'none';
+    cashOutSection.style.display = 'none';
+    addMoneySection.style.display = 'block';
+})
+
+//Cash Out button
+document.getElementById('cash-out').addEventListener('click', function () {
+    latestPaymentSection.style.display = 'none';
+    addMoneySection.style.display = 'none';
+    cashOutSection.style.display = 'block';
+})
+
+//Add Money System
+document.getElementById('add-btn').addEventListener('click', function () {
+    let number = parseInt(document.getElementById('ac-number').value);
+    let pin = parseInt(document.getElementById('pin').value);
+    let mainAmount = parseInt(document.getElementById('main-amount').textContent);
+    let addAmount = parseInt(document.getElementById('add-amount').value);
+    if (number.length = 11 || pin === 123) {
+        mainAmount = mainAmount + addAmount;
+        document.getElementById('main-amount').textContent = mainAmount;
+        alert("Money added successfully.")
+    }
+    else {
+        alert("The number or pin is wrong.")
+    }
+})
+
+//Withdraw Money System
+document.getElementById('withdraw-btn').addEventListener('click', function () {
+    let number = parseInt(document.getElementById('ac-number').value);
+    let pin = parseInt(document.getElementById('pin').value);
+    let witdhdrawAmount = parseInt(document.getElementById('withdraw-amount').value);
+    let mainAmount = parseInt(document.getElementById('main-amount').textContent);
+    if (number.length = 11 || pin === 123) {
+        if (witdhdrawAmount <= mainAmount) {
+            document.getElementById('main-amount').textContent = mainAmount - witdhdrawAmount;
+            alert("Money withdraw successfully.")
         }
-        else{
-            alert("Incorrect password.");
+        else {
+            alert("Insufficient balance.")
         }
     }
-    else{
-        alert("Phone number must be 11 digits long.");
+    else {
+        alert("The number or pin is wrong.")
     }
 })
